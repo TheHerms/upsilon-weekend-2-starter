ecd29ab4a80da3a1f3eda71aa89a00be264292bd
Tyler Herman <tylerherman@Tylers-MacBook-Pro.local>
2017-01-13 16:25:07 -0600

    initial commit

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,30 @@
-$(document).ready(function(){
+$(function(){
+
+}
+$('button').on('click',function() {
+
+
+});
+
+
+  function() {
     $.ajax({
       type: "GET",
       url: "/data",
-      success: function(data){
+      success: function(response){
+
+        response.forEach(function(person) {
+
+        }
         console.log(data);
       }
     });
 });
+function appendDom(person) {
+  var $personDiv = ('<div class="person"></div>');
+  $personDiv.append('<p>'+ person.name +'</p>');
+  $personDiv.append('<p>' +person.githubUserName+ '</p>');
+  $personDiv.append('<p>' + person.shoutout + '</p>');
+
+  $('#classSlideShow')append($personDiv);
+}
