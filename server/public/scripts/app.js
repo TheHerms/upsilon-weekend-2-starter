7af3d25c7414d71bb8bc64ff563d525e974b88f1
Tyler Herman <tylerherman@Tylers-MacBook-Pro.local>
2017-01-16 08:19:42 -0600

    Initial commit of Carousel Project

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,30 +1,81 @@
-$(function(){
+var currentStudent = 0;
+var displayedStudent = 0;
 
-}
-$('button').on('click',function() {
+$(document).ready(function(){
+  $.ajax({
+    type: "GET",
+    url: "/data",
+    success: function(data) {
+      console.log(data);
+      var student=data[0];
+      console.log(student);
+      showStudent(student);
 
 
-});
 
+    $('.people').on('click', function() {
+      var index = $(this).attr('id');
+      console.log(currentStudent);
 
-  function() {
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(response){
-
-        response.forEach(function(person) {
-
-        }
-        console.log(data);
-      }
+      showStudent(currentStudent);
     });
-});
-function appendDom(person) {
-  var $personDiv = ('<div class="person"></div>');
-  $personDiv.append('<p>'+ person.name +'</p>');
-  $personDiv.append('<p>' +person.githubUserName+ '</p>');
-  $personDiv.append('<p>' + person.shoutout + '</p>');
 
-  $('#classSlideShow')append($personDiv);
-}
+    $('#next').on('click', function() {
+      console.log("Next button was clicked");
+      $('.highlighted').removeClass('highlighted');
+      if(currentStudent<15) {
+        currentStudent = Number(currentStudent) + 1;
+        var student=data[currentStudent]
+        showStudent(student);
+      } else {
+        currentStudent = 0;
+        var student=data[currentStudent]
+        showStudent(student);
+      }
+
+    });
+
+    $('#prev').on('click', function() {
+      console.log("Prev button was clicked");
+      $('highlighted').removeClass('highlighted');
+      if(currentStudent>0) {
+        currentStudent = Number(currentStudent) -1;
+        var student=data[currentStudent]
+        showStudent(student);
+      } else {
+        currentStudent = 16;
+        showStudent(student);
+      }
+
+    });
+
+    function showStudent(c) {
+      // $.ajax({
+        // type: "GET",
+        // url: "/data",
+        // success: function(data){
+          $('.highlighted').removeClass('highlighted');
+          console.log('GET /data returns', data);
+          $('#stage').fadeOut('fast');
+          $('#stage').empty();
+          displayedStudent = c;
+          //console.log(displayedStudent);
+          $('#stage').fadeIn(2000);
+          appendDom(displayedStudent);
+
+          $('#'+ currentStudent).addClass('highlighted');
+        // }
+      // });
+};
+
+    function appendDom(student) {
+      var $studentDiv = $('<div class="student"></div>');
+      $studentDiv.append('<h2>' + student.name + '</h2>');
+      $studentDiv.append('<p>' + student.githubUserName + '</p>');
+      $studentDiv.append('<p>' + student.shoutout + '</p>');
+      //console.log(person);
+      $('#stage').append($studentDiv);
+    };
+  }
+  });
+});
